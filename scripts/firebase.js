67be15e2652f19a14a51f73c63eda39d2ba9ce52
sawnts <yosawnts@gmail.com>
2026-01-30 17:51:12 -0800

    Debug: log replyingTo state on admin post

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -256,6 +256,8 @@ export async function postAsOwner() {
     const message = messageInput?.value.trim();
     if (!message) return;
 
+    console.log('postAsOwner - replyingTo:', replyingTo);
+
     const msgData = {
         name: 'santi',
         message: message,
@@ -268,6 +270,7 @@ export async function postAsOwner() {
         msgData.replyTo = replyingTo.id;
         msgData.replyToName = replyingTo.name;
         msgData.replyToText = replyingTo.text;
+        console.log('postAsOwner - adding reply data:', msgData);
     }
 
     window.db.ref('guestbook').push(msgData).then(() => {
